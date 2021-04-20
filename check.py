4c1d01752c7db4d7e803344631c7614e22661225
17Peeyush <peeyushkushwaha17@gmail.com>
2021-04-20 12:41:43 +0530

    removed check

--- check.py
+++
@@ -1,6 +0,0 @@
-import textsummarize
-
-if __name__=="__main__":
-    text3 = "tf-idf stands for Term frequency-inverse document frequency. The tf-idf weight is a weight often used in information retrieval and text mining. Variations of the tf-idf weighting scheme are often used by search engines in scoring and ranking a document’s relevance given a query. This weight is a statistical measure used to evaluate how important a word is to a document in a collection or corpus. The importance increases proportionally to the number of times a word appears in the document but is offset by the frequency of the word in the corpus (data-set)."
-    x=textsummarize.textSummarize(text3,2)
-    print(x)
